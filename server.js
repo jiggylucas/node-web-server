3592470ffa52690d61e0357286fc0c8c11d9d749
Michael Glozman <melissaglozman@melissas-air.lan>
2019-01-04 19:25:44 -0500

    Setup start script and Heroku port

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -23,7 +25,7 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   res.render('maintenence.hbs', {
-    
+
   })
 })
 
@@ -54,6 +56,6 @@ app.get('/about', (req, res) => {
 
   })
 })
-app.listen(3000, () => {
-  console.log('successfully hosting on port 3000')
+app.listen(port, () => {
+  console.log(`successfully hosting on port ${port}`)
 });
